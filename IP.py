38db8b98851142e7a541d96276460b3a6f620ede
Dilshad Ali <ghaznvi737@gmail.com>
2018-11-23 16:07:12 +0500

    Update IP.py

--- IP.py
+++ IP.py
@@ -2,6 +2,7 @@
 # Developed by: DILSHAD ALI CHATTHA
 # E-mail:       ghaznvi737@gmail.com
 # GIT:          https://github.com/Dilshad737
+# BLOG:         https://dilshad737.blogspot.com/
 # MOB:          +923023331087
 
 import socket    
