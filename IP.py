cc767b824257fdc30dfefd5eddb3aa740ae3a4ca
Dilshad Ali <ghaznvi737@gmail.com>
2018-11-23 02:57:12 -0800

    Add files via upload

---
+++ IP.py
@@ -0,0 +1,11 @@
+# Python Program to Get IP Address
+# Developed by: DILSHAD ALI CHATTHA
+# E-mail:       ghaznvi737@gmail.com
+# GIT:          https://github.com/Dilshad737
+# MOB:          +923023331087
+
+import socket    
+hostname = socket.gethostname()    
+IPAddr = socket.gethostbyname(hostname)    
+print("Your Computer Name is:" + hostname)    
+print("Your Computer IP Address is:" + IPAddr)  
